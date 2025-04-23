4dbd8a678bb460838f4e3ffef0db24cfaf20b68f
KoushikBinary01 <159457401+KoushikBinary01@users.noreply.github.com>
2025-04-23 23:48:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const { connectToDB, insertSchoolToDB, getSchoolsFromDB, getGeoLocationBasedOnIP, createTable } = require('./Utility');
